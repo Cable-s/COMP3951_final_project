7c6a790c4630dd12c0614cc6bc51d00ec8e9aeec
Caleb <caleb.s.janzen@gmail.com>
2025-03-24 12:33:02 -0700

    add documentation and remove redundancies

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -2,90 +2,149 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 using UnityEngine.UIElements;
 
+///This module is for the various buildings that the player can build.
+///<author>Caleb Janzen</author>
+
+///<summary>
+/// The building context that interacts with Unity.
+///</summary>
 public class buildingContext : MonoBehaviour
 {
     private IBuilding building;
     [SerializeField] internal TileBase waterBuildingTile, forestTileBuilding, mountainTileBuilding, grasslandTileBuilding;
-
+       
+    /// <summary>
+    /// unity builtin Start method
+    /// </summary>
     public void Start()
     {
+        //each building static TileBase must be added here beacuse they are not MonoBehaviour and cannot have [SerializeField]s
         ForestBuilding.tile = forestTileBuilding;
         WaterBuilding.tile = waterBuildingTile;
         MountainBuilding.tile = mountainTileBuilding;
         GrasslandBuilding.tile = grasslandTileBuilding;
     }
 
-    public TileBase getWaterBuildingTile()
-    {
-        return waterBuildingTile;
-    }
-
-    public TileBase getForestBuildingTile()
-    {
-        return forestTileBuilding;
-    }
-
-    public TileBase getMountainBuildingTile()
-    {
-        return mountainTileBuilding;
-    }
-
-    public TileBase getGrasslandBuildingTile()
-    {
-        return grasslandTileBuilding;
-    }
-
+    /// <summary>
+    /// set the current building for the context.
+    /// </summary>
+    /// <param name="newBuilding">An IBuilding</param>
     public void setBuilding(IBuilding newBuilding)
     {
         this.building = newBuilding;
     }
 
+    /// <summary>
+    /// Add the current building to the tilemap.
+    /// </summary>
+    /// <param name="tilePosition">The position of the building</param>
+    /// <param name="buildingMap">The tilemap for buildings that the building will be added to.</param>
     public void addBuildingToTile(Vector3Int tilePosition, Tilemap buildingMap)
     {
         this.building.addBuildingToTile(tilePosition, buildingMap);
     }
 }
 
+/// <summary>
+/// The IBuilding interface.
+/// </summary>
 public interface IBuilding
 {
+    /// <summary>
+    /// Static TileBase field for the building 'sprite'.
+    /// </summary>
     public static TileBase tile
     {
         get;
         set;
     }
+
+    /// <summary>
+    /// Add the current building to the tilemap.
+    /// </summary>
+    /// <param name="tilePosition">The position of the building</param>
+    /// <param name="buildingMap">The tilemap for buildings that the building will be added to.</param>
     public void addBuildingToTile(Vector3Int tilePosition, Tilemap buildingMap);
 }
 
+/// <summary>
+/// The building that get built on a forest.
+/// </summary>
 public class ForestBuilding : IBuilding
 {
+    /// <summary>
+    /// Static TileBase field for the building 'sprite'.
+    /// </summary>
     public static TileBase tile { get; set; }
+
+    /// <summary>
+    /// Add the current building to the tilemap.
+    /// </summary>
+    /// <param name="tilePosition">The position of the building</param>
+    /// <param name="buildingMap">The tilemap for buildings that the building will be added to.</param>
     public void addBuildingToTile(Vector3Int tilePosition, Tilemap buildingMap)
     {
         buildingMap.SetTile(tilePosition, tile);
     }
 }
 
+/// <summary>
+/// The building that gets built on water.
+/// </summary>
 public class WaterBuilding : IBuilding
 {
+    /// <summary>
+    /// Static TileBase field for the building 'sprite'.
+    /// </summary>
     public static TileBase tile { get; set; }
+
+    /// <summary>
+    /// Add the current building to the tilemap.
+    /// </summary>
+    /// <param name="tilePosition">The position of the building</param>
+    /// <param name="buildingMap">The tilemap for buildings that the building will be added to.</param>
     public void addBuildingToTile(Vector3Int tilePosition, Tilemap buildingMap)
     {
         buildingMap.SetTile(tilePosition, tile);
     }
 }
 
+/// <summary>
+/// The building that get built on mountains.
+/// </summary>
 public class MountainBuilding : IBuilding
 {
+    /// <summary>
+    /// Static TileBase field for the building 'sprite'.
+    /// </summary>
     public static TileBase tile { get; set; }
+
+    /// <summary>
+    /// Add the current building to the tilemap.
+    /// </summary>
+    /// <param name="tilePosition">The position of the building</param>
+    /// <param name="buildingMap">The tilemap for buildings that the building will be added to.</param>
     public void addBuildingToTile(Vector3Int tilePosition, Tilemap buildingMap)
     {
         buildingMap.SetTile(tilePosition, tile) ;
     }
 }
 
+/// <summary>
+/// The building that gets built on grasslands.
+/// </summary>
 public class GrasslandBuilding : IBuilding
 {
+    /// <summary>
+    /// Static TileBase field for the building 'sprite'.
+    /// </summary>
     public static TileBase tile { get; set; }
+
+    /// <summary>
+    /// Add the current building to the tilemap.
+    /// </summary>
+    /// <param name="tilePosition">The position of the building</param>
+    /// <param name="buildingMap">The tilemap for buildings that the building will be added to.</param>
     public void addBuildingToTile(Vector3Int tilePosition, Tilemap buildingMap)
     {
         buildingMap.SetTile(tilePosition, tile);
