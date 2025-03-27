1a7271a92e5b6371e4df65c6e6860a10c58e7201
Tanner Parkes <Tanner.lp9@gmail.com>
2025-03-27 08:11:26 -0700

    Added pause menu and scripts to controll button behaviour. Need to add to onClick in Unity editor. The methods were not appearing in dropdown menus.

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Script for the MainMenu Behaviour. 
+/// Created by Tanner Parkes
+/// Referenced: (Make your MAIN MENU Quickly! | Unity UI Tutorial For Beginners) https://www.youtube.com/watch?v=DX7HyN7oJjE 
+/// </summary>
 public class MainMenu : MonoBehaviour
 {
+
+    //Method for Start Button to change the scene to the ingame scene.
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(0);
     }
 
+    //Method for the Quit button to exit the application.
     public void QuitGame()
     {
         Application.Quit();
---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Script for controlling in game PauseMenu behaviour.
+/// Created by Tanner Parkes
+/// Referenced: (PAUSE MENU in Unity) https://www.youtube.com/watch?v=JivuXdrIHK0
+/// </summary>
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+    public GameObject PauseMenuUI;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+        
+    }
+
+    /// <summary>
+    /// Pause Method, brings up the PauseMenuUI.
+    /// </summary>
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        GameIsPaused = true;
+    }
+
+    /// <summary>
+    /// Resume Method, hides the PauseMenu and resumes game.
+    /// </summary>
+    void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        GameIsPaused = false;
+    }
+
+    /// <summary>
+    /// MainMenu Method, returns the user to the main menu.
+    /// </summary>
+    void MainMenu()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    /// <summary>
+    /// Quit method, allows the user to exit the application from in the game.
+    /// </summary>
+    void Quit()
+    {
+        Application.Quit();
+    }
+}
