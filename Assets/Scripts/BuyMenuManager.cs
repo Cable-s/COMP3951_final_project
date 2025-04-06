8a2b3a57d7bb13094eab0e7da031e8b7c0ef7a83
Caleb <caleb.s.janzen@gmail.com>
2025-04-06 12:18:30 -0700

    fix being able to place buildings on top of eachother by making the buymenu close when you buy a building

--- Assets/Scripts/BuyMenuManager.cs
+++ Assets/Scripts/BuyMenuManager.cs
@@ -123,6 +123,7 @@ public class BuyMenuManager : MonoBehaviour
     {
         //Call building manager add building
         buildingManager.AddBuilding(buildingName, tilePosition);
+        this.CloseBuyMenu();
     }
 
     public void CloseBuyMenu()
